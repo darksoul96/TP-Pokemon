b902f6d91ea28fead61d22dbd9dfca08c1b5e71b
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2020-04-21 19:36:45 -0300

    BORRADAS LAS 3 CLASES

--- src/ClaseAbstractaPrueba.java
+++
@@ -1,7 +0,0 @@
-
-public abstract class ClaseAbstractaPrueba {
-	String prueba = "Prueba Clase abstracta";
-	String otrocambio = "Otro Cambio";
-	String tercercambio = "tercer cambio";
-	double numero = 1;
-}
