0cac1d4296de80735308d799edda8966e2c38fd2
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2020-04-21 19:15:16 -0300

    Tercer cambio

--- src/ClaseAbstractaPrueba.java
+++ src/ClaseAbstractaPrueba.java
@@ -2,5 +2,6 @@
 public abstract class ClaseAbstractaPrueba {
 	String prueba ="Prueba Clase abstracta";
 	String otrocambio = "Otro Cambio";
+	String tercercambio = "tercer cambio";
 	double numero = 1;
 }
