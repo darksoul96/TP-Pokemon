718581f253a5d677b1b23c9f7d898653e3941093
mati <matiasalmeida546@gmail.com>
2020-04-21 21:11:05 -0300

    Modificacion arena

--- src/Arena.java
+++ src/Arena.java
@@ -4,17 +4,27 @@ public class Arena {
 
 	private static Arena arena;
 	private ArrayList<Entrenador> entrenadores;
+	private ArrayList<Enfrentamiento> enfrentamientos;
 
 	private Arena() {
 		this.entrenadores = new ArrayList<Entrenador>();
+		this.enfrentamientos = new ArrayList<Enfrentamiento>();
 	}
-	
+
 	public static Arena getInstanceSingleton() {
-		if(arena==null)
+		if (arena == null)
 			arena = new Arena();
 		else
 			System.out.println("Ya existe instancia de arena");
 		return arena;
+	}
+
+	public ArrayList<Entrenador> getEntrenadores() {
+		return entrenadores;
+	}
+
+	public ArrayList<Enfrentamiento> getEnfrentamientos() {
+		return enfrentamientos;
 	}
 
 }
