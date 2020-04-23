9c9918ec13b27af4d04e9f3505de807cfa356f7d
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2020-04-23 19:05:48 -0300

    clase pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,5 +1,17 @@
 
 public abstract class Pokemon {
+	protected String nombre;
+	protected double experiencia = 0;
+	protected double escudo;
+	protected double vitalidad;
+	protected double ataque;
 
-	
+	public Pokemon(String nombre, double escudo, double vitalidad, double ataque) {
+		super();
+		this.nombre = nombre;
+		this.escudo = escudo;
+		this.vitalidad = vitalidad;
+		this.ataque = ataque;
+	}
+
 }
