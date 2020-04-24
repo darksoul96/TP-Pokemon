296cee32d63e19c535198b99a81bef6d060c798f
mati <matiasalmeida546@gmail.com>
2020-04-24 17:03:48 -0300

    Cambios en enfrentamiento

--- src/modelo/Enfrentamiento.java
+++ src/modelo/Enfrentamiento.java
@@ -10,16 +10,25 @@ public class Enfrentamiento {
 	private String ganador;
 	private String perdedor;
 	
-	public Enfrentamiento(Entrenador entrenadorUno, Entrenador entrenadorDos, String ganador, String perdedor) {
+	public Enfrentamiento(Entrenador entrenadorUno, Entrenador entrenadorDos) {
 		super();
 		this.entrenadorUno = entrenadorUno;
 		this.entrenadorDos = entrenadorDos;
-		this.ganador = ganador;
-		this.perdedor = perdedor;
 	}
 
 	public void batalla() {
+		// lo ideal seria que el ganador y el perdedor se setee despues de batalla y no en el constructor
 		Random r = new Random();
 		int rng = r.nextInt(10); // flip a coin, de 0 a 4 ataca primero E1, sino ataca primero E2.
 	}
+
+	public String getGanador() {
+		return ganador;
+	}
+
+	public String getPerdedor() {
+		return perdedor;
+	}
+	
+	
 }
