de40f81afebafb9f66ae5a8cceee1fd4f79bdb89
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2020-04-25 06:53:14 -0300

    Prueba con cosas.

--- src/modelo/Prueba.java
+++ src/modelo/Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Bienvenido al torneo Pokemon \n");
 		System.out.println("Ingrese la cantidad de participantes en el torneo: ");
-		while (participantes < 2) {
+		while (participantes<2) {
 			try {
 				cadena = scanner.nextLine();
 				participantes = Integer.parseInt(cadena);
@@ -26,19 +26,19 @@ public class Prueba {
 			}
 		}
 		System.out.println("Participaran " + participantes + " entrenadores");
+		torneo.setCantidadDeParticipantes(participantes);
 		for (int i = 1; i <= participantes; i++) {
 			System.out.println("Ingrese el nombre del entrenador: " + i);
 			nombre = scanner.nextLine();
 			Entrenador e1 = EntrenadorFactory.fabricaEntrenador(nombre);
-			torneo.agregarEntrenador(e1);
 			System.out.println("Ingrese cuantos pokemones tiene " + nombre);
+			cadena = scanner.nextLine();
 			try {
-				cadena = scanner.nextLine();
+
 				cantidad = Integer.parseInt(cadena);
 
 			} catch (NumberFormatException e) {
 				System.out.println("Numero no valido, ingrese un entero positivo mayor a 1");
-
 			}
 			for (int j = 1; j <= cantidad; j++) {
 				System.out.println("Ingrese el nombre del pokemon " + j);
@@ -69,6 +69,7 @@ public class Prueba {
 
 			}
 		}
+		System.out.println(torneo.getEntrenadores().size());
 		System.out.println(torneo);
 		System.out.println("Participantes correctamente cargados, inciando torneo...");
 		torneo.arrancaTorneo();
