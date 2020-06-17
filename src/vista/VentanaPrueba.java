4b066d134d19afbc7061721d3f8872727dfd7f1c
mati <matiasalmeida546@gmail.com>
2020-06-17 20:20:10 -0300

    ventana base

--- src/vista/VentanaPrueba.java
+++ src/vista/VentanaPrueba.java
@@ -6,12 +6,32 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 public class VentanaPrueba extends JFrame {
 
 	private JPanel contentPane;
-	private JPanel panelCentralEntrenadores;
-	private JPanel panelFasesDeJuego;
+	private JPanel panelCentral;
+	private JPanel panelEntrenadoresPokemones;
+	private JPanel panelMedio;
+	private JPanel panelBatallas;
+	private JPanel panelPokemones;
+	private JPanel panelEntrenadores;
+	private JScrollPane scrollPane;
+	private JList listPokemones;
+	private JLabel lblPokemones;
+	private JScrollPane scrollPane_1;
+	private JLabel lblEntrenadores;
+	private JList listEntrenadores;
+	private JScrollPane scrollPane_2;
+	private JLabel lblNewLabel;
+	private JList listBatallas;
+	private JPanel panelSur;
 
 	/**
 	 * Launch the application.
@@ -40,11 +60,60 @@ public class VentanaPrueba extends JFrame {
 		this.contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(this.contentPane);
 		
-		this.panelCentralEntrenadores = new JPanel();
-		this.contentPane.add(this.panelCentralEntrenadores, BorderLayout.CENTER);
+		this.panelCentral = new JPanel();
+		this.contentPane.add(this.panelCentral, BorderLayout.CENTER);
+		this.panelCentral.setLayout(new GridLayout(0, 3, 0, 0));
 		
-		this.panelFasesDeJuego = new JPanel();
-		this.contentPane.add(this.panelFasesDeJuego, BorderLayout.EAST);
+		this.panelEntrenadoresPokemones = new JPanel();
+		this.panelCentral.add(this.panelEntrenadoresPokemones);
+		this.panelEntrenadoresPokemones.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		this.panelEntrenadores = new JPanel();
+		this.panelEntrenadoresPokemones.add(this.panelEntrenadores);
+		this.panelEntrenadores.setLayout(new BorderLayout(0, 0));
+		
+		this.scrollPane_1 = new JScrollPane();
+		this.panelEntrenadores.add(this.scrollPane_1, BorderLayout.CENTER);
+		
+		this.lblEntrenadores = new JLabel("Entrenadores");
+		this.scrollPane_1.setColumnHeaderView(this.lblEntrenadores);
+		
+		this.listEntrenadores = new JList();
+		this.scrollPane_1.setViewportView(this.listEntrenadores);
+		
+		this.panelPokemones = new JPanel();
+		this.panelEntrenadoresPokemones.add(this.panelPokemones);
+		this.panelPokemones.setLayout(new BorderLayout(0, 0));
+		
+		this.scrollPane = new JScrollPane();
+		this.panelPokemones.add(this.scrollPane, BorderLayout.CENTER);
+		
+		this.listPokemones = new JList();
+		this.scrollPane.setViewportView(this.listPokemones);
+		
+		this.lblPokemones = new JLabel("Pokemones");
+		this.scrollPane.setColumnHeaderView(this.lblPokemones);
+		
+		this.panelMedio = new JPanel();
+		this.panelCentral.add(this.panelMedio);
+		
+		this.panelBatallas = new JPanel();
+		this.panelCentral.add(this.panelBatallas);
+		this.panelBatallas.setLayout(new BorderLayout(0, 0));
+		
+		this.scrollPane_2 = new JScrollPane();
+		this.panelBatallas.add(this.scrollPane_2, BorderLayout.CENTER);
+		
+		this.lblNewLabel = new JLabel("Batallas");
+		this.scrollPane_2.setColumnHeaderView(this.lblNewLabel);
+		
+		this.listBatallas = new JList();
+		this.scrollPane_2.setViewportView(this.listBatallas);
+		
+		this.panelSur = new JPanel();
+		this.contentPane.add(this.panelSur, BorderLayout.SOUTH);
+		
+		//this.btnNewButton.setVisible(false);
 	}
 
 }
