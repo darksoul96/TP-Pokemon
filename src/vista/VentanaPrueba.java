ee09604737ad676fae2e5e5da627edd97dcf3dfd
mati <matiasalmeida546@gmail.com>
2020-06-17 19:18:17 -0300

    agregado paquete vista

---
+++ src/vista/VentanaPrueba.java
@@ -0,0 +1,50 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class VentanaPrueba extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel panelCentralEntrenadores;
+	private JPanel panelFasesDeJuego;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrueba frame = new VentanaPrueba();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaPrueba() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 846, 556);
+		this.contentPane = new JPanel();
+		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(this.contentPane);
+		
+		this.panelCentralEntrenadores = new JPanel();
+		this.contentPane.add(this.panelCentralEntrenadores, BorderLayout.CENTER);
+		
+		this.panelFasesDeJuego = new JPanel();
+		this.contentPane.add(this.panelFasesDeJuego, BorderLayout.EAST);
+	}
+
+}
