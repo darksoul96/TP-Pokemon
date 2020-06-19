7e8e96a736f14bcc013b72f75b0b46740030e843
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2020-06-19 19:42:40 -0300

    GeneraEnfrentamiento en grupos

--- src/modelo/Grupo.java
+++ src/modelo/Grupo.java
@@ -9,6 +9,7 @@ public class Grupo {
 	private String Nunero;
 	private boolean lleno = false;
 	boolean grupoFinalizado = false;
+	int cantidadEf =0;
 
 	public void agregarEntrenador(Entrenador e) {
 		if (lleno != true)
@@ -18,9 +19,36 @@ public class Grupo {
 		}
 	}
 
-	synchronized public Enfrentamiento generaEnfrentamiento(Enfrentamiento ef) {
-		
-		return null;
+	public synchronized  Enfrentamiento generaEnfrentamiento() {
+		Enfrentamiento e = null;
+		switch(this.cantidadEf) {
+		case 0:
+			e = new Enfrentamiento(integrantes.get(0),integrantes.get(1));
+			this.cantidadEf++;
+			break;
+		case 1:
+			e = new Enfrentamiento(integrantes.get(2),integrantes.get(3));
+			this.cantidadEf++;
+			break;
+		case 2:
+			e = new Enfrentamiento(integrantes.get(0),integrantes.get(2));
+			this.cantidadEf++;
+			break;
+		case 3:
+			e = new Enfrentamiento(integrantes.get(1),integrantes.get(3));
+			this.cantidadEf++;
+			break;
+		case 4:
+			e = new Enfrentamiento(integrantes.get(0),integrantes.get(3));
+			this.cantidadEf++;
+			break;
+		case 5:
+			e = new Enfrentamiento(integrantes.get(1),integrantes.get(2));
+			this.cantidadEf++;
+			this.grupoFinalizado = true;
+			break;
+		}
+		return e;
 	}
 
 	public boolean isLleno() {
